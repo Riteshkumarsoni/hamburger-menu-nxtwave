4801dcfa41847f5a4593ebd4a0546bf20488512e
Riteshkumarsoni <riteshsoni062022@gmail.com>
2025-12-12 10:45:21 +0530

    Adding Contact Components

---
+++ src/components/Contact/index.js
@@ -0,0 +1,17 @@
+import Header from '../Header'
+import './index.css'
+
+const Contact = () => (
+  <>
+    <Header />
+    <div className="common-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/home-lg-img.png"
+        alt="home"
+        className="home-img"
+      />
+    </div>
+  </>
+)
+
+export default Contact
